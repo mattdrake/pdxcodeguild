970a5109e66c49ef659d0f281dc4abd1223ba95b
Matthew Drake <mattsdrake@gmail.com>
2014-08-28 10:02:20 -0700

    Changed highlited story ending. Added comments. Added colors to break up prompting sentences.

--- madlib_color.py
+++ madlib_color.py
@@ -7,7 +7,7 @@ dislike = raw_input('\033[0;34mWhat do you really hate?\033[0;m ')
 
 #Story that input will be put into.
 story = """
-\033[1;42mHello, my name is %(name)s. I'm from %(location)s. 
+\033[1;41mHello, my name is %(name)s. I'm from %(location)s. 
 I arrived here by %(transportation)s. In my spare time I like to %(hobby)s. 
 I really hate %(dislike)s.\033[1;m"""
 
