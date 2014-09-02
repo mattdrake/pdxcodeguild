91e449779750ac5a8a2593518abe9e9f67825671
mattdrake <mattsdrake@gmail.com>
2014-09-02 15:59:11 -0700

    paired programming. using while andfor loops

---
+++ every_other_for_loop.py
@@ -0,0 +1,9 @@
+__author__ = 'drake'
+
+#user input question for number to increment
+question = input("Enter a number. ")
+
+#create loop to increment every other number
+for num in question:
+    num += 2
+    print(num)
