22190548f30f4ce09a06b561d1cc283977930560
mattdrake <mattsdrake@gmail.com>
2014-09-04 16:14:43 -0700

    updated primes paire project comments

--- primes.py
+++ primes.py
@@ -3,13 +3,14 @@ __author__ = 'drake'
 # lists out all prime numbers between 2 and integer given by user
 
 # asks user for input for integer
-question = input("Please enter a number. ") + 1
+question = input("Please enter a number. ")
 
 # creates new list with no elements to add prime numbers to later
 results = []
 # creates a range of numbers up to and including integer
 for i in range(2, question):
-    for l in range(2, i - 1):
+# creates another range for each integer from range above to be checked if divisible by 2 evenly
+    for l in range(2, i):
         if i % l == 0:
 # if the number is not prime it will be skipped
             break
