b5a91f51f3283e84e687b3d7e13655399f595f42
mattdrake <mattsdrake@gmail.com>
2014-09-19 13:12:51 -0700

    added colors to output to break up reading lines

--- rock_paper.py
+++ rock_paper.py
@@ -8,11 +8,12 @@ from termcolor import *
 #create class that will hold defined game function
 class Game():
     def __init__(self):
+        #starting with a score of zero for each of the following attributes to increment and display at the end of game
         self.player_score = 0
         self.computer_score = 0
         self.ties = 0
 
-        def playables():
+        def playable():
             cprint("Rock\n"
                    "Spock\n"
                    "Paper\n"
@@ -21,10 +22,11 @@ class Game():
             #create list of possible answers to pull from
 
         def match():
-            playables()
+            playable()
             possible = ['Scissors', 'Paper', 'Rock', 'Lizard', 'Spock']
 
-            #create dictionary to correspond with what element from "possible" list corresponds with what the action does by indices
+            #create dictionary to correspond with what element from "possible" list
+            # corresponds with what the action does by indices
 
             d = {'01': 'Cuts', '12': 'Covers', '23': 'Crushes',
                  '34': 'Poisons', '40': 'Smashes', '03': 'Decapitate', '31': 'Eats',
@@ -32,7 +34,8 @@ class Game():
 
             #get input from player on which choice they want to go with
 
-            player_choice = raw_input(colored("Pick which one you want to play! Try to beat the computer! : ", 'yellow')).capitalize()
+            player_choice = raw_input(colored("Pick which one you want to play! "
+                                              "Try to beat the computer! : ", 'yellow')).capitalize()
 
             #create option to prompt user for input if they choose something that is not an available option
 
@@ -49,19 +52,19 @@ class Game():
             #declare the computer's choice to be the result of the random_choice variable
             computer_choice = possible[random_choice]
 
-            print 'Player Picks: '+player_choice
-            print 'Computer Picks: '+possible[random_choice] + '\n'
+            print 'Player Picks: '+colored(player_choice, 'green')
+            print 'Computer Picks: '+colored(possible[random_choice], 'red') + '\n'
 
-            """todo add scoing options to the game. give number of computer wins,
-            player wins, and ties after the player has decided to quit the game."""
+            """todo add scoring options to the game. give number of computer wins,
+            player wins, and ties after the player has decided to quit the game.(done)"""
             """todo add doc strings to functions"""
 
             #determining if the choices of both the user and computer are the same, resulting in a "tie"
             if random_choice == index:
-                print ("It's a tie! ")
+                cprint ("It's a tie! ", 'red', 'on_grey')
 
                 #prompting user if they want to continue playing or exit the game
-                play_again = raw_input("Would you like to play again? ").lower()
+                play_again = raw_input(colored("Would you like to play again? ", 'yellow')).lower()
                 self.ties += 1
                 if play_again in ['y', 'yes']:
                     match()
@@ -75,30 +78,41 @@ class Game():
                     #if the computer's choice is first, the computer would win and the following code executed.
 
                     result = d[str(random_choice) + str(index)]
-                    print(computer_choice + ' ' + result + ' ' + player_choice + '. Computer wins! ')
+                    print computer_choice + ' ' + result + ' ' + player_choice + '.' + \
+                          colored(' Computer wins!', 'red', 'on_grey')
                     self.computer_score += 1
                     play_again = raw_input(colored("Would you like to play again? ", 'yellow')).lower()
                     if play_again in ['y', 'yes']:
                         match()
                     else:
                         cprint("Thank you for playing! ", 'yellow')
-                        print("your score was {0}, and the computer score was {1}".format(self.player_score,
-                                                                                          self.computer_score))
+                        cprint("Your score was " + str(self.player_score) + ", and the computer's score was "
+                               + str(self.computer_score),  'blue')
+                        if self.ties == 1:
+                            cprint("There was just " + str(self.ties) + " tie!")
+                        else:
+                            cprint("There were " + str(self.ties) + " ties! ", 'yellow')
+
 
                 except:
                     #if the user's choice is the first of the index of the dictionary above,
                     # the following code would be executed.
 
                     result = d[str(index) + str(random_choice)]
-                    print player_choice + ' ' + result + ' ' + computer_choice + '. Player Wins! '
+                    print player_choice + ' ' + result + ' ' + computer_choice + '.' + \
+                          colored(' Player Wins!', 'green')
                     self.player_score += 1
-                    play_again = raw_input("Would you like to play again? ").lower()
+                    play_again = raw_input(colored("Would you like to play again? ", 'yellow')).lower()
                     if play_again in ['y', 'yes']:
                         match()
                     else:
                         cprint("Thank you for playing! ", 'yellow')
-                        print("your score was {0}, and the computer score was {1}".format(self.player_score, self.computer_score))
-                        print("there were " + str(self.ties) + " ties! ")
+                        cprint("Your score was " + str(self.player_score) + ", and the computer's score was "
+                               + str(self.computer_score),  'blue')
+                        if self.ties == 1:
+                            cprint("There was just " + str(self.ties) + " tie!")
+                        else:
+                            cprint("There were " + str(self.ties) + " ties! ", 'yellow')
 
         match()
 
