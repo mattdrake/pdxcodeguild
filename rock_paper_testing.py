87941c7029f0ad3061ec6d633a19e7fb1c539b61
Matthew Drake <mattsdrake@gmail.com>
2014-10-20 15:49:17 -0700

    Delete rock_paper_testing.py

--- rock_paper_testing.py
+++
@@ -1,8 +0,0 @@
-d = {'01': 'Cuts', '02': 'Covers', '03': 'Crushes',
-                 '04': 'Poisons', '05': 'Smashes', '06': 'Decapitate', '07': 'Eats',
-                 '08': 'Disproves', '09': 'Vaporizes', '10': 'Crushes'}
-
-
-d = {'01': 'Cuts', '12': 'Covers', '23': 'Crushes',
-                 '34': 'Poisons', '40': 'Smashes', '04': 'Decapitate', '31': 'Eats',
-                 '14': 'Disproves', '42': 'Vaporizes', '20': 'Crushes'}
